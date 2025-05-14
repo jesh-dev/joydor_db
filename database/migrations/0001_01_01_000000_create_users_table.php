8f809c6a7b8a0985e403c99b2e1edebbdb172d76
jesh-dev <jeshrunlaw@gmail.com>
2025-05-14 10:38:28 +0100

    deleting duplicated email_verified_at

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
             $table->string('phone_number',15);
             $table->enum('gender', ['male', 'female', 'other'])
             ->default('male');
