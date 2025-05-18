5f61608fc8db8a37d9982c5f4b18813fdf65b6ec
jesh-dev <jeshrunlaw@gmail.com>
2025-05-18 13:38:04 +0100

    add user=verification updated

--- resources/views/emails/user-verification.blade.php
+++ resources/views/emails/user-verification.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <div class='bg-orange-500 '>
-        <h1 class=' bg-gray-500 ml-20 align-center rounded-xl flex flex-col w-100'>Hello {{ $user->name }}</h1>
+        <h1 class=' bg-gray-500 ml-20 align-center rounded-xl flex flex-col w-100'>Hello {{ $user->firstname . $user->lastname }}</h1>
         <h3 class='px-5 text-md '>This is your verification code:</h3>
         <p class='px-5 font-bold text-2xl'>{{ $user->verification_code }}</p>
     </div>
